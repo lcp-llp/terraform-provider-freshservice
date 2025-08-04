d196bb48e4962ddc69fc58e54a055c937cc7fd1c
Luke Dowsett <luke.dowsett@lcp.uk.com>
2025-08-04 10:33:51 +0100

    fix: Update module path for Freshservice provider in main.go

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/lcp-llp/terraform-registry-freshservice/provider"
+	"github.com/lcp-llp/terraform-provider-freshservice/provider"
 )
 
 func main() {
