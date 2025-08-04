6650114b65bdb7e3553c9f3e6cf81fa52d02ede0
Luke Dowsett <luke.dowsett@lcp.uk.com>
2025-08-04 10:17:47 +0100

    fix(provider): Correct import path for Freshservice provider in main.go

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/lcp-llp/terraform-registry-vmnameregistry/provider"
+	"github.com/lcp-llp/terraform-registry-freshservice/provider"
 )
 
 func main() {
